b6780f5f3b1fb7e2f8d401e1f1256935708ec329
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2019-04-13 17:59:30 +0530

    Improved video picker

--- lib/components/video_picker.dart
+++ lib/components/video_picker.dart
@@ -1,16 +1,12 @@
 import 'dart:async';
 import 'dart:io';
-import 'package:redux/redux.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:chewie/chewie.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:video_player/video_player.dart';
 import 'package:file_chooser/file_chooser.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-
-import 'package:fastter_dart/store/state.dart';
-import 'package:fastter_dart/store/uploads.dart';
+import 'package:chewie/chewie.dart';
+import 'package:video_player/video_player.dart';
+import '../helpers/storage.dart';
 
 class VideoPickerUploader {
   VideoPickerUploader({
@@ -41,8 +37,8 @@ class VideoPickerUploader {
   }
 }
 
-class VideoPickerUploaderWidget extends StatelessWidget {
-  const VideoPickerUploaderWidget({
+class _VideoPickerUploaderWidget extends StatefulWidget {
+  const _VideoPickerUploaderWidget({
     @required this.context,
     @required this.value,
     @required this.storagePath,
@@ -55,63 +51,49 @@ class VideoPickerUploaderWidget extends StatelessWidget {
   final String storagePath; // Firebase storage path
   final void Function(String) onChange;
   final void Function(dynamic) onError;
+
   @override
-  Widget build(BuildContext context) =>
-      StoreConnector<AppState, Store<AppState>>(
-        converter: (store) => store,
-        builder: (context, store) => _VideoPickerUploaderWidget(
-              context: context,
-              value: value,
-              storagePath: storagePath,
-              onChange: onChange,
-              onError: onError,
-              upload: (file, fileName) {
-                final action = UploadStart(file, fileName);
-                store.dispatch(action);
-                return action.completer.future.then((data) => data.url);
-              },
-            ),
-      );
+  _VideoPickerUploaderWidgetState createState() =>
+      _VideoPickerUploaderWidgetState();
 }
 
-class _VideoPickerUploaderWidget extends StatelessWidget {
-  _VideoPickerUploaderWidget({
-    @required this.context,
-    @required this.value,
-    @required this.storagePath,
-    @required this.onChange,
-    @required this.onError,
-    @required this.upload,
-  });
-
-  final BuildContext context;
-  final String value; // Url of the image
-  final String storagePath; // Firebase storage path
-  final void Function(String) onChange;
-  final void Function(dynamic) onError;
-  final Future<String> Function(File, String) upload;
-
+class _VideoPickerUploaderWidgetState
+    extends State<_VideoPickerUploaderWidget> {
   File _image;
+  StorageUploadTask _uploadTask;
+
+  void _onChange(String url) {
+    widget.onChange(url);
+    Navigator.of(widget.context).pop();
+  }
 
   Future _takeProfileVideo() async {
     if (Platform.isAndroid || Platform.isIOS) {
-      _image = await ImagePicker.pickVideo(source: ImageSource.camera);
+      final image = await ImagePicker.pickVideo(source: ImageSource.camera);
+      setState(() {
+        _image = image;
+      });
     } else {}
   }
 
   Future _selectProfileVideo() async {
     if (Platform.isAndroid || Platform.isIOS) {
-      _image = await ImagePicker.pickVideo(source: ImageSource.gallery);
+      final image = await ImagePicker.pickVideo(source: ImageSource.gallery);
+      setState(() {
+        _image = image;
+      });
     } else {
       final completer = Completer<void>();
       showOpenPanel(
         (FileChooserResult fileChooserResult, filepaths) {
           if (filepaths.isNotEmpty) {
-            _image = File(filepaths[0]);
+            setState(() {
+              _image = File(filepaths[0]);
+            });
             completer.complete();
           }
         },
-        allowedFileTypes: ['jpg', 'png'],
+        allowedFileTypes: ['mp4', 'avi'],
         allowsMultipleSelection: false,
         canSelectDirectories: false,
       );
@@ -122,19 +104,25 @@ class _VideoPickerUploaderWidget extends StatelessWidget {
   Future<void> _uploadProfileVideo() async {
     try {
       if (Platform.isAndroid || Platform.isIOS) {
-        final ref = FirebaseStorage.instance.ref().child(storagePath);
-        final uploadTask = ref.putFile(_image);
+        final ref = FirebaseStorage.instance.ref().child(widget.storagePath);
+        setState(() {
+          _uploadTask = ref.putFile(_image);
+        });
+        _uploadTask.events.listen((event) {
+          setState(() {
+            _uploadTask.lastSnapshot = event.snapshot;
+          });
+        });
         final String downloadUrl =
-            await (await uploadTask.onComplete).ref.getDownloadURL();
+            await (await _uploadTask.onComplete).ref.getDownloadURL();
 
-        onChange(downloadUrl);
+        _onChange(downloadUrl);
       } else {
-        final url = await upload(_image, storagePath);
-        onChange(url);
-        // TODO(prijindal): Implement firebase storage upload rest api
+        final url = await uploadFirebase(widget.storagePath, _image.path);
+        _onChange(url);
       }
     } catch (error) {
-      onError(error);
+      widget.onError(error);
     }
   }
 
@@ -148,9 +136,9 @@ class _VideoPickerUploaderWidget extends StatelessWidget {
     await _uploadProfileVideo();
   }
 
-  Future<void> _urlSelectVideo() async {
+  Future<void> _urlInputVideo() async {
     final urlController = TextEditingController(
-      text: value,
+      text: widget.value,
     );
     final shouldUpdate = await showDialog<bool>(
       context: context,
@@ -176,85 +164,67 @@ class _VideoPickerUploaderWidget extends StatelessWidget {
           ),
     );
     if (shouldUpdate) {
-      onChange(urlController.text);
+      _onChange(urlController.text);
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    final widgets = <Widget>[
+      widget.value == null || widget.value.isEmpty
+          ? const Icon(
+              Icons.person,
+              size: 200,
+            )
+          : Chewie(
+              controller: ChewieController(
+                videoPlayerController: VideoPlayerController.network(
+                  widget.value,
+                ),
+                aspectRatio: 3 / 2,
+              ),
+            ),
+    ];
+    if (Platform.isAndroid || Platform.isIOS) {
+      widgets.add(
+        ListTile(
+          title: const Text('Take a video'),
+          onTap: _takeAndUploadVideo,
+        ),
+      );
+    }
+    if (_uploadTask != null &&
+        _uploadTask.isInProgress &&
+        _uploadTask.lastSnapshot != null) {
+      final percentageUploaded = (_uploadTask.lastSnapshot.bytesTransferred /
+              _uploadTask.lastSnapshot.totalByteCount) *
+          100;
+      widgets.add(
+        ListTile(
+          title: Text('Uploaded ${percentageUploaded.round()} %'),
+        ),
+      );
+    }
+    widgets.addAll(
+      [
+        ListTile(
+          title: const Text('Select a video'),
+          onTap: _selectAndUploadVideo,
+        ),
+        ListTile(
+          title: const Text('Input a url'),
+          onTap: _urlInputVideo,
+        ),
+        ListTile(
+          title: const Text('Remove video'),
+          onTap: () => _onChange(null),
+        ),
+      ],
+    );
+
     return SimpleDialog(
       title: const Text('Change Profile Video'),
-      children: (Platform.isAndroid || Platform.isIOS)
-          ? <Widget>[
-              value == null || value.isEmpty
-                  ? const Icon(
-                      Icons.person,
-                      size: 200,
-                    )
-                  : Chewie(
-                      controller: ChewieController(
-                        videoPlayerController: VideoPlayerController.network(
-                          value,
-                        ),
-                        aspectRatio: 3 / 2,
-                      ),
-                    ),
-              ListTile(
-                title: const Text('Take a picture'),
-                onTap: () {
-                  Navigator.of(context).pop();
-                  _takeAndUploadVideo();
-                },
-              ),
-              ListTile(
-                title: const Text('Select a picture'),
-                onTap: () {
-                  Navigator.of(context).pop();
-                  _selectAndUploadVideo();
-                },
-              ),
-              ListTile(
-                title: const Text('Input a url'),
-                onTap: () {
-                  Navigator.of(context).pop();
-                  _urlSelectVideo();
-                },
-              ),
-              ListTile(
-                title: const Text('Remove picture'),
-                onTap: () {
-                  onChange(null);
-                },
-              ),
-            ]
-          : [
-              value == null || value.isEmpty
-                  ? const Icon(
-                      Icons.person,
-                      size: 200,
-                    )
-                  : Chewie(
-                      controller: ChewieController(
-                        videoPlayerController: VideoPlayerController.network(
-                          value,
-                        ),
-                        aspectRatio: 3 / 2,
-                      ),
-                    ),
-              ListTile(
-                title: const Text('Input a url'),
-                onTap: () {
-                  Navigator.of(context).pop();
-                  _urlSelectVideo();
-                },
-              ),
-              ListTile(
-                title: const Text('Remove picture'),
-                onTap: () {
-                  onChange(null);
-                },
-              ),
-            ],
+      children: widgets,
     );
   }
 }
