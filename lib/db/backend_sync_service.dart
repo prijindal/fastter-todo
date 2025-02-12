2757560c78deeb8439a2e7820fe423fd2e7189e2
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2025-02-12 16:48:56 +0530

    Fixed periodic send queue data

--- lib/db/backend_sync_service.dart
+++ lib/db/backend_sync_service.dart
@@ -44,8 +44,7 @@ class BackendSyncService {
 
   Future<void> init() async {
     await _sendQueueData();
-    _timer =
-        Timer.periodic(Duration(milliseconds: 100), (_) => _sendQueueData());
+    _timer = Timer.periodic(Duration(seconds: 1), (_) => _sendQueueData());
     await fetchHistoryWrapper();
     // After running _sendQueueData once, create a timer to run it perioidically
     // Fetch data from server, using list operation
