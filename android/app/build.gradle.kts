f761546371b8e452c3ce935e78612ba80600d5ae
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2026-03-18 11:04:32 +0530

    Upgraded java compatiblity to 21

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,12 +22,12 @@ android {
     compileOptions {
         // Flag to enable support for the new language APIs
         isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_21.toString()
     }
 
     val versionPropsFile = rootProject.file("version.properties")
